44e11914ede1b38ea447356ff1c97fa74e15763a
ukaiiaku-maker <ukaiiaku@gmail.com>
2026-07-17 08:58:45 -0700

    Expose v9.10.4.1 audit progress version

--- arrhenius_fracture/__init__.py
+++ arrhenius_fracture/__init__.py
@@ -71,4 +71,4 @@ MovingProcessZoneState = _MPZStateV95
 # only timestep selection; it does not alter any constitutive rate or state law.
 from . import reduced_campaign_front_v9104_timestep_guard as _v9104_timestep_guard
 
-__version__ = '0.9.10.4'
+__version__ = '0.9.10.4.1'
