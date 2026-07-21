0ae0caeb25bd4fbb6badd91fd4c597259ca6d14c
ukaiiaku-maker <ukaiiaku@gmail.com>
2026-07-20 19:42:33 -0700

    Add public API for v9.12 emergent GND campaign

---
+++ arrhenius_fracture/emergent_gnd_dbtt_v912.py
@@ -0,0 +1,4 @@
+"""Public API for the isolated v9.12 emergent-GND DBTT campaign."""
+from .emergent_gnd_types_v912 import *
+from .emergent_gnd_state_v912 import EmergentGNDState
+from .emergent_gnd_campaign_v912 import *
